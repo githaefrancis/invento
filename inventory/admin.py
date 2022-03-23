b1b2880812172d892b23195a5d201e6a33f20029
Francis Githae <francis.githae@student.moringaschool.com>
2022-03-23 21:26:09 +0300

    register all models on the admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Department,Employee
+from .models import Department,Employee,Equipment,SupplyConsumption,Allocation,Category
 # Register your models here.
 
 admin.site.register(Department,)
 admin.site.register(Employee)
+admin.site.register(Allocation)
+admin.site.register(SupplyConsumption)
+admin.site.register(Equipment)
+admin.site.register(Category)
